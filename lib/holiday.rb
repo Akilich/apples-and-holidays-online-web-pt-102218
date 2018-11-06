cae9371357215681b3f58df729999aebb907d647
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 11:39:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -3,6 +3,11 @@ require 'pry'
 def holiday_supplies_hash
 holiday_supplies[:summer][:fourth_of_july][1]
 end
+
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
+  
   
  def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, value|
